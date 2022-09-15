b163f17e6986d1d60540d10806db3f14f4aa6d57
Aseef Ahmed <aahmed@linz.govt.nz>
2022-09-16 00:39:53 +1200

    changes

--- microservices/sqs_message_reader/message.py
+++ microservices/sqs_message_reader/message.py
@@ -12,7 +12,7 @@ from fpdf import FPDF
 
 SQS_NAME="orders"
 S3_BUCKET="orders324323342"
-dynamodb_table="orders_test"
+DYNAMODB_TABLE="orders_test"
 
 sqs = boto3.client('sqs')
 dynamodb = boto3.resource('dynamodb')
@@ -25,13 +25,8 @@ account_number = caller['Account']
 
 session = boto3.session.Session()
 aws_region = session.region_name
-#https://sqs.ap-southeast-2.amazonaws.com/416125572371/orders
 queueUrl = "https://sqs.{0}.amazonaws.com/{1}/{2}".format(aws_region, account_number, SQS_NAME)
-print(queueUrl)
-s3_bucket_arn = "arn:aws:s3:::orders324323342"
-print(s3_bucket_arn)
-s3_bucket = s3_bucket_arn.split(':::')[1]
-print(s3_bucket)
+s3_bucket_arn = "arn:aws:s3:::{0}".format(S3_BUCKET)
 
 while True:
     response = sqs.receive_message(
@@ -51,6 +46,7 @@ while True:
         for _ in range(1):
             opId = randint(500000000, 999999999)
         name = message_json['Customer']
+        email = message_json['Email']
         OrderId = message_json['OrderId']
         OrderDate = message_json['OrderDate']
         OrderAmount = message_json['OrderAmount']
@@ -74,6 +70,7 @@ while True:
         pdf.add_page()
         pdf.set_font('Arial', '', 10)
         pdf.cell(0, 10, 'Name: ' + name, 0, 1, 'L')
+        pdf.cell(0, 10, 'Email: ' + email, 0, 1, 'L')
         pdf.cell(0, 10, 'Order ID: ' + str(OrderId), 0, 1, 'L')
         pdf.cell(0, 10, 'Order Date: ' + OrderDate, 0, 1, 'L')
         pdf.cell(0, 10, 'Order Amount: ' + OrderAmount, 0, 1, 'L')
@@ -81,17 +78,16 @@ while True:
         pdf.cell(0, 10, 'Thank you for you order', 0, 1, 'C')
 
    
-        # pdf.cell(0, 10, 'Confirmation code: ' + confirCode, 0, 1, 'L')
-        # pdf.cell(0, 10, 'Purchase date: ' + paydate, 0, 1, 'L')
         pdf.output(str(opId) + '.pdf', 'F')
 
-        upload_s3 = s3.upload_file(str(opId) + '.pdf', s3_bucket, str(opId) + '.pdf')
+        upload_s3 = s3.upload_file(str(opId) + '.pdf', S3_BUCKET, str(opId) + '.pdf')
 
-        ddb_table = dynamodb.Table(dynamodb_table)
+        ddb_table = dynamodb.Table(DYNAMODB_TABLE)
         putItem = ddb_table.put_item(
             Item={
                 'id': opId,
                 'name': name,
+                'email': email,
                 'OrderId': OrderId,
                 'OrderDate': OrderDate,
                 'OrderAmount': OrderAmount,
